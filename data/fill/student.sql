afae28365a24ead836538bb8c8beeb3dabb594c6
Brano <b.vavrincek@gmail.com>
2023-02-17 16:39:21 +0100

    database tables

---
+++ data/fill/student.sql
@@ -0,0 +1,5 @@
+insert into student (id,meno,priezvisko) values ('bde492e7-1902-4e34-a304-6e740d6729b2','Jozo','Mrkvicka');
+insert into student (id,meno,priezvisko) values ('db2e4104-cdd2-4f27-abb6-a5be15a36003','Pavol','Brokolica');
+insert into student (id,meno,priezvisko) values ('ddfb64cc-a701-403c-8715-eef96ad34b23','Fero','Citron');
+insert into student (id,meno,priezvisko) values ('66f3a05d-130e-4193-aabd-af3506bd1305','Martin','Buk');
+insert into student (id,meno,priezvisko) values ('e9100ede-df10-4a35-bb32-726737778124','Laco','Strike');
