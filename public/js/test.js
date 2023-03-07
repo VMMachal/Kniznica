fd6a0e60c7dec6a96445dc742b7f83434b58b1b0
Brano <b.vavrincek@gmail.com>
2023-03-07 18:22:13 +0100

    Pokusy

--- public/js/test.js
+++ public/js/test.js
@@ -1,21 +1,30 @@
+function sendGetRequest (url) {
+    let promise = fetch(url, {
+        method: "GET",
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      return data;
+    })
+    .catch((error) => {
+     console.error("Error:", error);
+    });
+    return promise;
+}
+
 let anchor = document.getElementById("testGet");
     anchor.addEventListener("click", (event) => {
         event.preventDefault();
+        
+        sendGetRequest("/api").then((data)=>{
+            console.log("@@@@@@@@@@@ cp 5000")
+            console.dir(data);
+        })
+
         console.log("click2");
         console.dir(event);
         const data = { username: "example" };
-
-        fetch("/pokux.txt", {
-            method: "GET",
-        })
-        .then((response) => response.text())
-        .then((data) => {
-          console.log("Success:", data);
-        })
-        .catch((error) => {
-         console.error("Error:", error);
-        });
-
     })
 let button = document.getElementById("testPost_button");
     button.addEventListener("click", (event) => {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
+//const {router} = require('./router/router')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.send('Hello World1!')
-})
+//app.use('/api', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
